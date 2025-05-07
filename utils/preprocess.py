2e4a4490fbe1fff8a5cfdbaed500a2778e969f5c
Harshtherocking <65885345+Harshtherocking@users.noreply.github.com>
2025-05-07 12:08:47 +0530

    fix

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -1,17 +1,19 @@
 import torch 
-import torchvision
-import PIL 
-import os 
-
+import cv2 as cv
 from PIL import Image
-
 import torchvision.transforms.functional as F
+import numpy as np
+
+BRIGHTNESS = 50
+CONTRAST = 30
+DIM = 4418
 
 def preprocess_image(images : list[str] | str )  -> torch.Tensor : 
     """
+    cropping
     min max normalization
     uint16 -> uint8
-    brightness = 50 and contrast = 30
+    brightness and contrast
     """
 
     if isinstance(images, str):
@@ -21,20 +23,19 @@ def preprocess_image(images : list[str] | str )  -> torch.Tensor :
 
     for image in images : 
         img = Image.open(image)
-        img = F.to_tensor(img)
-        img = F.center_crop(img, [4418,4418])
+        img = np.array(img, dtype=np.uint16)
         
         # min max normalization
-        # min_all not impplemented for UInt16
         min_ = img.min()
         max_ = img.max()
         img = (img - min_) / (max_ - min_) * 255
-        img = img.type(torch.uint8)
+        img.astype(np.uint8)
 
-        #brightness and contrast
-        img = F.adjust_contrast(img, 30)
-        img = F.adjust_brightness(img, 50)
+        # brightness and contrast
+        img = cv.convertScaleAbs(img, alpha=BRIGHTNESS, beta=CONTRAST)
 
+        img = F.to_tensor(img)
+        img = F.center_crop(img, [DIM, DIM])
         imgs.append(img)
 
 
